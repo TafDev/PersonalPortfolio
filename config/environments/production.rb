be546346e72d507c91437277349ecd649ea382c1
TafDev <chombot@hotmail.com>
2017-03-22 23:59:38 +0000

    fixing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = {host: 'http://www.tafdev.com'}
-  config.action_mailer.delivery_method = smtp
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
